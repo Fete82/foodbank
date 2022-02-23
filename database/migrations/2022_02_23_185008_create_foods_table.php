bac215242c1dba518108209ae407996794747dfb
Edvin Lindahl <fete.edvin@gmail.com>
2022-02-23 19:51:01 +0100

    new migration, proper name

--- database/migrations/2022_02_23_185008_create_foods_table.php
+++ database/migrations/2022_02_23_185008_create_foods_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class FoodTable extends Migration
+class CreateFoodsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class FoodTable extends Migration
      */
     public function up()
     {
-        Schema::create('food', function (Blueprint $table) {
+        Schema::create('foods', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('description');
@@ -28,6 +28,6 @@ class FoodTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('food');
+        Schema::dropIfExists('foods');
     }
 }
