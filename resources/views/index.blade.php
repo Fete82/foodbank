ed29cb5a763c4da009b465b778ca784cc8386c0a
Edvin Lindahl <fete.edvin@gmail.com>
2022-02-23 21:15:49 +0100

    fixed refactoring

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,9 @@
 @extends('layouts.layout')
 
-    <!-- Main content -->
+@section('home')
+<div class="container mt-5 text-center">
+    <h1>Foodbank</h1>
+    <h3>Probably The Most Nutritional Food In The World</h3>
 
-    <div class="container mt-5 text-center">
-        <h1>Foodbank</h1>
-        <h3>Probably The Most Nutritional Food In The World</h3>
-
-    </div>
-
-</body>
-
-</html>
+</div>
+@endsection
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -39,4 +39,5 @@
     </nav>
 </body>
 @yield('main')
+@yield('home')
 </html>
