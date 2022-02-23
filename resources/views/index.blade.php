2d903d5b66770781390e37d9a7a3b44e5a0ecd50
Edvin Lindahl <fete.edvin@gmail.com>
2022-02-23 21:16:43 +0100

    code style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.layout')
 
 @section('home')
-<div class="container mt-5 text-center">
-    <h1>Foodbank</h1>
-    <h3>Probably The Most Nutritional Food In The World</h3>
-
-</div>
+    <!-- Welcoming page -->
+    <div class="container mt-5 text-center">
+        <h1>Foodbank</h1>
+        <h3>Probably The Most Nutritional Food In The World</h3>
+    </div>
 @endsection
