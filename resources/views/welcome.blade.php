2f4e64d186cc9a76cee681955dcae07ab68be6d0
Edvin Lindahl <fete.edvin@gmail.com>
2022-02-28 16:58:10 +0100

    background fixed, kinda

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<body class="home-body">
+
+<body class="welcome-wrapper-background">
     <!-- Welcoming page -->
     <div class="align-middle text-center mt-5 text-dark">
         <h1 class="display-1 p-3">Foodbank</h1>
         <h1 class="display-6">Cows are mostly food</h1>
 
         <div>
-            <a class="btn btn-lg btn-success mt-5" href='food'>Browse Recipes</a>
-            <a class="btn btn-lg btn-primary mt-5" href='/food/create'>Add New Recipe</a>
+            <a class="btn btn-lg btn-success mt-5 mx-1" href='food'>Browse Recipes</a>
+            <a class="btn btn-lg btn-primary mt-5 mx-1" href='/food/create'>Add New Recipe</a>
         </div>
     </div>
     <footer class="footer-fixed text-center fixed-bottom">
-        <p>&copy; 2022 Edvin Lindahl</p>
+        <p class="text-light">&copy; 2022 Edvin Lindahl</p>
     </footer>
-</body>
+</div>
+
 @endsection
