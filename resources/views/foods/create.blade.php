4fc45a52af8fd20f702e1e45806bed6a38b4fe7e
Edvin Lindahl <fete.edvin@gmail.com>
2022-02-28 13:30:56 +0100

    updated structure for create form

--- resources/views/foods/create.blade.php
+++ resources/views/foods/create.blade.php
@@ -13,29 +13,31 @@
                 <!-- cross site refresh forgery -->
                 <div>
                     @csrf
-                    <label class="mt-5" for="name">Name of Meal:</label>
+                    <label class="mt-5" for="name">Course name:</label>
                     <br>
                     <input type="text" id="input" name="name">
                 </div>
                 <div>
-                    <label class="mt-3" for="description">Description of Meal:</label>
+                    <label class="mt-3" for="description">Description of meal:</label>
                     <br>
                     <textarea type="textarea" id="input" name="description"></textarea>
                 </div>
                 <div>
                     <fieldset>
-                        <label class="mt-3">Ingredients/Instructions:</label>
+                        <label class="mt-3">Ingredients:</label>
                         <br>
-                        <input type="text" class="input mt-1" name="ingredients[]"><br>
-                        <input type="text" class="input mt-1" name="ingredients[]"><br>
-                        <input type="text" class="input mt-1" name="ingredients[]"><br>
-                        <input type="text" class="input mt-1" name="ingredients[]"><br>
-                        <input type="text" class="input mt-1" name="ingredients[]"><br>
-                        <input type="text" class="input mt-1" name="ingredients[]"><br>
-
-
-
+                        <input type="text" class="input mt-2" name="ingredients[]"><br>
+                        <input type="text" class="input mt-2" name="ingredients[]"><br>
+                        <input type="text" class="input mt-2" name="ingredients[]"><br>
+                        <input type="text" class="input mt-2" name="ingredients[]"><br>
+                        <input type="text" class="input mt-2" name="ingredients[]"><br>
+                        <input type="text" class="input mt-2" name="ingredients[]"><br>
                     </fieldset>
+                </div>
+                <div>
+                <label class="mt-3" for="instructions">Cooking instructions:</label>
+                    <br>
+                    <textarea type="textarea" id="input" name="instructions"></textarea>
                 </div>
                 <input type="submit" class="btn btn-lg btn-primary m-5">
             </form>
